d803287dbe59ca6baf71fbcda4b355e9ce4646fc
Novapixel1010 <msocial@brwfamily.com>
2025-04-28 15:41:26 -0600

    underscore

--- lang/cz/landing.php
+++ lang/cz/landing.php
@@ -14,6 +14,6 @@ return [
 	*/
 	'gallery' => 'Gallery',
 	'access_gallery' => 'Access the gallery',
-	'Powered_by_lychee' => 'Powered_by_lychee',
+	'Powered_by_lychee' => 'Powered by lychee',
 	'copyright' => 'All images on this website are subject to copyright by %1$s © %2$s',
 ];
